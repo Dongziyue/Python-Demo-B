fa1e967987078c1152a23b72b4432706befcbbd9
thu <mingfei.net@gmail.com>
2018-11-19 17:22:34 +0800

    Update

--- string.py
+++ string.py
@@ -40,3 +40,49 @@ print(s.endswith('o', 0, 5))
 print(s.startswith('e', 1, 5))
 
 print(s.find(','))
+
+print(s.isalnum())  # al=alpha num=number
+
+print(s.isalpha())
+
+print(s.isdigit())
+
+s = "A一二三"
+print(s.islower())
+print(s.isupper())
+
+s = "123a"
+
+print(s.isnumeric())
+
+s = '    '
+print(s.isspace())
+
+s = 'Hello, World!'
+print(s.lower())
+print(s.upper())
+print(s.swapcase())
+
+s = '       Hello, World!      '
+print(s.lstrip())
+print(s.rstrip())
+print(s.strip())  # trim()
+
+
+s = "HelloWorld一"
+
+print(min(s))
+print(max(s))
+
+s = 'Hello, World!'
+print(s.replace('l', 'L', 2))
+
+print(type(123))
+print(type(str(123)))
+
+s = '123'
+
+print(s.isdigit())
+print(s.isnumeric())
+print(s.isdecimal())
+
