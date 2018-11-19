58594579d0263092f8aca51f1a92fdaae809993a
thu <mingfei.net@gmail.com>
2018-11-19 16:24:12 +0800

    Update

---
+++ string.py
@@ -0,0 +1,30 @@
+s = 'Hello, World!'
+
+print(s[0])
+
+print(s[len(s) - 1])
+
+print(s[-1])
+
+s1 = 'Hello, '
+s2 = 'World!'
+
+print(s1 + s2)
+
+print(s1, s2)
+
+print(s * 2)
+
+print(s[0:-1])
+
+print(s[0:5:3])  # <c:foreach  step="1"> 步长
+
+print('Hello' in s)
+
+print(True)
+
+print(False)
+
+s = R'Hello, \nWorld!'
+
+print(s)
