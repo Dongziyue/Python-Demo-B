20f370ff373113f3874d0d8d50d6bfdeca093552
thu <mingfei.net@gmail.com>
2018-11-19 11:26:33 +0800

    Update

---
+++ comment.py
@@ -0,0 +1,13 @@
+# print('test') Ctrl + /
+
+'''
+print('test1')
+print('test2')
+print('test3')
+'''
+
+"""
+print('test...')
+"""
+
+print('test4')
---
+++ data_type.py
@@ -0,0 +1,42 @@
+# Number
+
+print(type(123))
+
+print(0xff)
+
+print(0x4e00)  # 一 unicode
+print(0x9fbb)  # 一 unicode
+
+print(21474836472147483647214748364721474836472147483647214748364721474836472147483647)
+
+print(1.234e4)
+
+print(type(1.234e4))
+
+print('abc')
+print("xyz")
+
+print('abc' + "xyz")
+
+print('abc', "xyz")
+
+print('hello'*3)
+
+print("Tom's name")  # Tom's name
+
+print('Tom\'s name')
+
+print(r'Tom\'s name')
+
+print('line\nanother line')
+
+# HTML <pre>
+print('''
+line...
+    another line...
+''')
+
+print(r'''
+line...\n
+    another line...
+''')
