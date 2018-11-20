cbf5c2a5fe1032174d976343b2d93d81bbb8868c
thu <mingfei.net@gmail.com>
2018-11-20 16:46:35 +0800

    Update

--- e_6.py
+++ e_6.py
@@ -1,7 +1,22 @@
 # 输入两个正整数 m 和 n，求其最大公约数和最小公倍数
 
-m = 10
-n = 100
+m = int(input('input int m: '))
+n = int(input('input int n: '))
 
-print(max(m, n))
-print(min(m, n))
+# min-max
+if m > n:
+    m, n = n, m
+
+print(m, n)
+
+# 最大公约数
+for i in range(min(m, n), 0, -1):
+    if m % i == 0 and n % i == 0:
+        # print(i)
+        break
+
+# 最小公倍数
+for i in range(max(m, n), m * n + 1):
+    if i % m == 0 and i % n == 0:
+        # print(i)
+        break
