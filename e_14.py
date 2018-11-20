d5bfd72a9dc5a61954d11e7320341ea26175792f
thu <mingfei.net@gmail.com>
2018-11-20 17:23:33 +0800

    Update

--- e_14.py
+++ e_14.py
@@ -1,7 +1,23 @@
 # 输入某年某月某日，判断这一天是这一年的第几天？
 # 闰年： 西元年份除以400余数为0的，或者除以4为余数0且除以100不为余数0的，为闰年。
 
+# year = int(input('input year: '))
+# month = int(input('input month: '))
+# day = int(input('input day: '))
+#
+# month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#
+# if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+#     month_days[1] = 29
+#
+# sum = 0
+# for i in range(0, month - 1):
+#     sum += month_days[i]
+#
+# sum += day
+#
+# print(sum)
 
-year = int(input('input year: '))
-month = int(input('input month: '))
-day = int(input('input day: '))
+import time
+
+print(time.gmtime()[7])
