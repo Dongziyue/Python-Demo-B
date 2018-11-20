3414eb76fbf869ec41c24f94a0737efc613f0db9
thu <mingfei.net@gmail.com>
2018-11-20 10:42:39 +0800

    Update

---
+++ if_test.py
@@ -0,0 +1,34 @@
+"""
+C
+if ()
+{
+    // ...
+}
+
+Java
+if () {
+    // ...
+}
+
+Python
+if ():
+    // ...
+
+"""
+
+score = 95
+
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 60:
+    print('C')
+else:
+    print('D')
+
+if score >= 60: print('passed...')
+
+# (condition)?value1:value2 三目运算符
+
+# value1 if condition else value2 Python
