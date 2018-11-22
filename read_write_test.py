fa7d222d09edc1d3a4eb02399f1897cbe2a4111a
thu <mingfei.net@gmail.com>
2018-11-22 14:37:12 +0800

    Update

---
+++ read_write_test.py
@@ -0,0 +1,39 @@
+# 中文
+
+
+# f = open('read_write_test.py', encoding='UTF-8')
+#
+# print(f.read())
+#
+# f.close()
+
+
+# with open('read_write_test.py', encoding='UTF-8') as f:
+#     print(f.read())
+
+
+# with open('read_write_test.py', encoding='UTF-8') as f:
+#     print(f.read(3))
+
+
+# with open('read_write_test.py', encoding='UTF-8') as f:
+#     line = f.readline()
+#     while line:
+#         print(line, end='')
+#         line = f.readline()
+
+
+# false_value_list = [False, 0, None, '', [], (), {}]
+#
+# for value in false_value_list:
+#     if value:
+#         print('True: ',  value)
+#     else:
+#         print('False: ',  value)
+
+
+# with open('read_write_test.py', encoding='UTF-8') as f:
+#     print(type(f))
+    # for x in f:
+    #     print(x, end='')
+
