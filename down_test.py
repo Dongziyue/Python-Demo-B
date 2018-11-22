bb9c43691614dba4c7b7ef4a0a6cd24e3cdce0fe
thu <mingfei.net@gmail.com>
2018-11-22 15:04:12 +0800

    Update

---
+++ down_test.py
@@ -0,0 +1,3 @@
+# 下载一个在线的视频文件存在本地
+
+# https://static.yximgs.com/s1/videos/www_main-059ce9beee.mp4
--- read_write_test.py
+++ read_write_test.py
@@ -37,3 +37,33 @@
     # for x in f:
     #     print(x, end='')
 
+# with open('read_write_test.py', encoding='UTF-8') as f:
+#     for line in f.readlines():
+#         print(line, end='')
+
+
+# with open('google.png', 'rb') as f:
+#     print(f.read())
+
+
+# from urllib.request import urlopen
+#
+# with urlopen('https://book.douban.com/subject/1005022/') as f:
+#     for line in f.readlines():
+#         print(line.decode('UTF-8'), end='')
+
+
+# with open('test.txt', 'w', encoding='UTF-8') as f:
+#     f.write('Hello, 你好!')
+
+
+# with open('test.txt', 'a', encoding='UTF-8') as f:
+#     f.write('Hello, 你好!')
+
+
+# with open('test-x.txt', 'x', encoding='UTF-8') as f:
+#     f.write('Hello, 你好!')
+
+with open('google.png', 'rb') as f1:
+    with open('logo.png', 'wb') as f2:
+        f2.write(f1.read())
