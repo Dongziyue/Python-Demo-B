8708ced8220fb40173652232dc894f68930bebf3
thu <mingfei.net@gmail.com>
2018-11-23 10:43:29 +0800

    Update

---
+++ abs_test.py
@@ -0,0 +1,2 @@
+# 自定义一个求绝对值的函数 fn_absolute_value，并验证
+
---
+++ function_test.py
@@ -0,0 +1,17 @@
+
+
+# print(abs(-1))
+# print(abs(-1, 0))
+# print(abs('a'))
+
+# print(min(1, 2))
+# print(max(1, 2, 3, 4, 5))
+
+# print(hex(17))  # 0x11
+
+# print(dir(__builtins__))
+# print(len(dir(__builtins__)))
+
+absolute = abs
+
+print(absolute(-1))
