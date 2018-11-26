1330689a4bb0c10d2ec688c87dc448cb785bbabc
thu <mingfei.net@gmail.com>
2018-11-26 11:13:15 +0800

    Update

--- oop_test.py
+++ oop_test.py
@@ -14,19 +14,83 @@
 # print(jerry.name)  # AttributeError
 
 
+# class Human(object):
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.__age = age
+#
+#     def get_name(self):
+#         return self.name
+#
+#     def get_age(self):
+#         return self.__age
+#
+#
+# tom = Human('Tom', 18)
+
+# print(tom.name)
+# print(tom.__age)
+
+# tom.__age = 19  # 对象可以自由绑定属性
+# print(tom.__age)
+# print(tom.get_age())
+
+
+# print(tom.name)
+# print(tom.age)
+#
+# print(tom.get_name())
+#
+# tom.name = 'Thomas'
+# print(tom.name)
+# print(tom.get_name())
+
+
 class Human(object):
-
-    def __init__(self, name, age):
+    def __init__(self, name):
         self.name = name
-        self.age = age
 
-    def get_name(self):
-        return self.name
+    def study(self):
+        print('Human can studying...')
 
 
-tom = Human('Tom', 18)
+class Chinese(Human):
 
-print(tom.name)
-print(tom.age)
+    def study(self):
+        print('Chinese can studying...')
 
-print(tom.get_name())
+
+zhangsan = Chinese('Zhang San')
+
+print(zhangsan.name)
+zhangsan.study()
+
+print(isinstance(zhangsan, Chinese))  # Java zhangsan.instanceof(Chinese);
+print(isinstance(zhangsan, Human))
+
+tom = Human('Tom')
+print(isinstance(tom, Human))
+print(isinstance(tom, Chinese))
+
+def fn_study(human):
+    human.study()
+
+fn_study(tom)
+fn_study(zhangsan)
+
+
+class Duck(object):
+    def study(self):
+        print('Duck is studying?')
+
+
+duck = Duck()
+fn_study(duck)
+
+
+
+
+
+
+
