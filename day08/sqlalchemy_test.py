7e006538acc8f3f2fc9c35e000aef8d9d138be22
thu <mingfei.net@gmail.com>
2018-11-28 17:57:33 +0800

    Update

--- day08/sqlalchemy_test.py
+++ day08/sqlalchemy_test.py
@@ -5,20 +5,38 @@
 # @FileName : sqlalchemy_test.py
 # @GitHub : https://github.com/thu/Python-Demo-B
 
-from sqlalchemy import Integer, String, Column, create_engine
+from sqlalchemy import Integer, String, Column, create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
 
 class User(Base):
-
     __tablename__ = 'user'
 
     id = Column(Integer(), autoincrement=True, primary_key=True)
     email = Column(String(255), nullable=False)
     password = Column(String(255), nullable=False)
+
+    books = relationship('Book')
+
+    def __str__(self):
+        return str(self.id) + ',\t' + self.email + ',\t' + self.password + '\n\tbooks:\n' + str(self.books)
+
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    id = Column(Integer(), autoincrement=True, primary_key=True)
+    title = Column(String(255), nullable=False)
+    author = Column(String(255), nullable=False)
+
+    userId = Column(Integer, ForeignKey('user.id'))
+
+    def __str__(self):
+        return str(self.id) + ',\t' + self.title + ',\t' + self.author
+
 
 # 数据库类型+数据库驱动名://账号:口令@服务器:端口号/数据库名
 engine = create_engine('mysql+mysqlconnector://root:system@localhost:3306/db_py')
@@ -36,6 +54,6 @@ session.commit()
 users = session.query(User).all()
 
 for user in users:
-    print(user.id, user.email, user.password)
+    print(user)
 
 session.close()
--- down_test.py
+++ down_test.py
@@ -3,7 +3,7 @@
 
 import urllib.request
 
-url = 'http://jandan.net/ooxx'
+url = 'http://you.163.com/item/detail?id=1505027&_stat_area=mod_limit_item_3&_stat_referer=index'
 
 with urllib.request.urlopen(url) as f:
     for line in f.readlines():
